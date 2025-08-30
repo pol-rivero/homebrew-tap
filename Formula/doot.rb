8459de94be3025d0b4ac9e2b2d8a94efe1041dd5
Formula Updater <formula-updater@example.com>
2025-08-30 12:48:30 +0000

    Update doot to version 0.6.4

--- Formula/doot.rb
+++ Formula/doot.rb
@@ -1,7 +1,7 @@
 class Doot < Formula
   desc     "Fast, simple and intuitive dotfiles manager that just gets the job done"
   homepage "https://github.com/pol-rivero/doot"
-  version  "0.6.3"
+  version  "0.6.4"
   license  "MIT"
   head     "https://github.com/pol-rivero/doot.git", branch: "main"
 
@@ -10,23 +10,23 @@ class Doot < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/pol-rivero/doot/releases/download/0.6.3/doot-darwin-arm64"
-      sha256 "ab005cd605e4d5fdd2f907323644d3ff44131ae5f43c2933bb9f3cdb9536e1f2"
+      url "https://github.com/pol-rivero/doot/releases/download/0.6.4/doot-darwin-arm64"
+      sha256 "b83969512303ff5d9bed36f275a7ccdc7d16df5cfc634cee35b00ec7e8717431"
     end
     on_intel do
-      url "https://github.com/pol-rivero/doot/releases/download/0.6.3/doot-darwin-x86_64"
-      sha256 "a9c8ae393c5de5886ffe984c27474521296a6bd732a9235a7ff98c8ef321d6a5"
+      url "https://github.com/pol-rivero/doot/releases/download/0.6.4/doot-darwin-x86_64"
+      sha256 "28a93f0cc6270684c924c37791244f11da11a8c8627c17926ad9eea4c7e006a3"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/pol-rivero/doot/releases/download/0.6.3/doot-linux-arm64"
-      sha256 "5b1ee4400ed080f779a30eef24642655c9095a827f80b0c263876cc64103972b"
+      url "https://github.com/pol-rivero/doot/releases/download/0.6.4/doot-linux-arm64"
+      sha256 "ae532b90e3f0a4fc6e39fce3a2a4bc17a9bb71c48f2aaa6643b915a0d4d1c8e7"
     end
     on_intel do
-      url "https://github.com/pol-rivero/doot/releases/download/0.6.3/doot-linux-x86_64"
-      sha256 "87292c8b45732bbb4d958f1bb09e77da9d9aa571bb0a2c5d8afb253739e7bf3e"
+      url "https://github.com/pol-rivero/doot/releases/download/0.6.4/doot-linux-x86_64"
+      sha256 "7ed2bcb0e281111807079898c5a266017206738de123bba089293b59742724f2"
     end
   end
 
