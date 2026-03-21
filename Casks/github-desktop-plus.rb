3980a0f01f2497f3b96c68545f6e98fdd705b455
Pol Rivero <65060696+pol-rivero@users.noreply.github.com>
2026-03-21 10:54:22 +0100

    Initial release of the github-desktop-plus cask

---
+++ Casks/github-desktop-plus.rb
@@ -0,0 +1,28 @@
+cask "github-desktop-plus" do
+  arch arm: "arm64", intel: "x64"
+
+  version "3.5.7.1"
+  sha256 arm:   "e7908e79958ee37c68f32af651dd0660d6e83f4beb4e2f71ff56355966f13d44",
+         intel: "00e8808a3168c8e3778368e91312573474615ee1e8eb13137305cab198699ca2"
+
+  url "https://github.com/pol-rivero/github-desktop-plus/releases/download/v#{version}/GitHubDesktopPlus-v#{version}-macOS-#{arch}.zip"
+  name "GitHub Desktop Plus"
+  desc "Fork of GitHub Desktop with extra features and improvements"
+  homepage "https://github.com/pol-rivero/github-desktop-plus"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :monterey"
+
+  app "GitHub Desktop Plus.app"
+  binary "#{appdir}/GitHub Desktop Plus.app/Contents/Resources/app/static/github.sh",
+         target: "github-desktop-plus-cli"
+
+  zap trash: [
+        "~/Library/Application Support/GitHub Desktop Plus",
+        "~/Library/Logs/GitHub Desktop Plus",
+      ]
+end
