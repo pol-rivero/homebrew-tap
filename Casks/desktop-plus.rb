a65e169a3c818a2f17cf0347169a4e0344a19b0d
Formula Updater <formula-updater@desktop-plus.org>
2026-07-25 16:30:04 +0000

    Update desktop-plus to version 3.6.4.0

--- Casks/desktop-plus.rb
+++ Casks/desktop-plus.rb
@@ -1,9 +1,9 @@
 cask "desktop-plus" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.6.3.1"
-  sha256 arm:   "344c97b9f4566037167434731e2e089f9d4042adb295ad1eb94e84c0e9347baa",
-         intel: "b26eadfd67a946b6c2bb55c1e717c5a2805dac35764b0375a8483469e3a2b7f5"
+  version "3.6.4.0"
+  sha256 arm:   "51806040ba4171861f692666fbceccaecbad41941f4baa0c6a0ed804e8c95e53",
+         intel: "27522a3ea328646f839e5880e7320da61d6fb5f0551c1911bbc7faca28d993dc"
 
   url "https://github.com/desktop-plus/desktop-plus/releases/download/v#{version}/DesktopPlus-v#{version}-macOS-#{arch}.zip"
   name "Desktop Plus"
